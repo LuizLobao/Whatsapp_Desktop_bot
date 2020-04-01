ccc6152a0f478c828b488f8981c341802a829969
Luiz Lobão <luiz@lobao.com>
2020-03-31 21:53:13 -0300

    open json and gets grupo names and messages

--- whatsapp_desk_bot.py
+++ whatsapp_desk_bot.py
@@ -1,9 +1,9 @@
-import os
-import time
+import os, json, time
 from pynput.keyboard import Key, Controller
 from pywinauto import application
 
 keyboard = Controller()
+
 
 def OpenWhatsappDesktop():
     """Open the Whastapp desktop application. Change the path before use."""
@@ -37,19 +37,38 @@ def SearchGroup(nome):
         print(e)
 
 
-OpenWhatsappDesktop()
-time.sleep(10)
+def OpenJson():
+    """Open json file with the Group Name and its Message and retur as a dict"""
+    # Path to Json
+    CWD = os.getcwd()
+    JSON_PATH = '%s/%s' % (CWD, 'lista_mensagens.json')
+    # Dictionary holding the groups and messages values
+    msgs = {}
+    # Open json and store to Dictionary
+    with open(JSON_PATH) as data_file:
+        msgs = json.load(data_file)
+    return(msgs)
 
-groups = ['Eu Sozinho', 'Teste Python']
-for group in groups:
-    SearchGroup(group)
-    horaatual = time.strftime('%d-%b-%Y %X')
+
+lista = OpenJson()
+for grupos in lista['lista_mensagens']:
+    print(grupos['grupo'], ' - ', grupos['mensagem'])
+
+OpenWhatsappDesktop()
+time.sleep(30)
+
+for grupos in lista['lista_mensagens']:
+    SearchGroup(grupos['grupo'])
+    
     #paste the image copied to memory before the program was started
     with keyboard.pressed(Key.ctrl):
         keyboard.press('v')
         keyboard.release('v')
-    time.sleep(1)
-    keyboard.type(f'Teste de bot em Python via Whatsapp DeskTop, {horaatual}.')
+    time.sleep(3)
+    #type de mesage and send it
+    m = grupos['mensagem']
+    horaatual = time.strftime('%d-%b-%Y %X')
+    keyboard.type(f'{m}. Hora de Envio: {horaatual}.')
     time.sleep(1)
     keyboard.press(Key.enter)
     keyboard.release(Key.enter)
